b47ec42b044ee9c87bf984f6c5929dada330cc0a
Christina Moulton <christina@teakmobile.com>
2016-04-02 13:27:12 -0300

    Added GitHubAPIManager file

---
+++ grokSwiftREST/GitHubAPIManager.swift
@@ -0,0 +1,15 @@
+//
+//  GitHubAPIManager.swift
+//  grokSwiftREST
+//
+//  Created by Christina Moulton on 2016-04-02.
+//  Copyright © 2016 Teak Mobile Inc. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class GitHubAPIManager {
+  
+}
