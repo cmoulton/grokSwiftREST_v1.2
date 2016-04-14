77b9dd2c5109e7bc8a6ab8d444aeec32f0f0168f
Christina Moulton <christina@teakmobile.com>
2016-04-14 17:16:30 -0300

    Added Gist model object class

--- grokSwiftREST/Gist.swift
+++ grokSwiftREST/Gist.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+import SwiftyJSON
+
+class Gist {
+  var id: String?
+  var description: String?
+  var ownerLogin: String?
+  var ownerAvatarURL: String?
+  var url: String?
+  
+  required init(json: JSON) {
+    self.description = json["description"].string
+    self.id = json["id"].string
+    self.ownerLogin = json["owner"]["login"].string
+    self.ownerAvatarURL = json["owner"]["avatar_url"].string
+    self.url = json["url"].string
+  }
+  
+  required init() {
+  }
+}
