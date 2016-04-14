edf2555c5614c68d651c831497d8a0d8fd2a0c7d
Christina Moulton <christina@teakmobile.com>
2016-04-14 17:19:50 -0300

    Swap array of objects to gists

--- grokSwiftREST/MasterViewController.swift
+++ grokSwiftREST/MasterViewController.swift
@@ -11,8 +11,7 @@ import UIKit
 class MasterViewController: UITableViewController {
 
   var detailViewController: DetailViewController? = nil
-  var objects = [AnyObject]()
-
+  var gists = [Gist]()
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -47,22 +46,29 @@ class MasterViewController: UITableViewController {
   }
 
   func insertNewObject(sender: AnyObject) {
-    objects.insert(NSDate(), atIndex: 0)
-    let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-    self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+    let alert = UIAlertController(title: "Not Implemented", message:
+      "Can't create new gists yet, will implement later",
+                                  preferredStyle: UIAlertControllerStyle.Alert)
+    alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default,
+      handler: nil))
+    self.presentViewController(alert, animated: true, completion: nil)
   }
 
   // MARK: - Segues
 
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
     if segue.identifier == "showDetail" {
-        if let indexPath = self.tableView.indexPathForSelectedRow {
-            let object = objects[indexPath.row] as! NSDate
-            let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-            controller.detailItem = object
-            controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
-            controller.navigationItem.leftItemsSupplementBackButton = true
+      if let indexPath = self.tableView.indexPathForSelectedRow {
+        let gist = gists[indexPath.row] as Gist
+        if let detailViewController = (segue.destinationViewController as!
+          UINavigationController).topViewController as?
+          DetailViewController {
+          detailViewController.detailItem = gist
+          detailViewController.navigationItem.leftBarButtonItem =
+            self.splitViewController?.displayModeButtonItem()
+          detailViewController.navigationItem.leftItemsSupplementBackButton = true
         }
+      }
     }
   }
 
@@ -73,31 +79,35 @@ class MasterViewController: UITableViewController {
   }
 
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return objects.count
+    return gists.count
   }
 
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-    let object = objects[indexPath.row] as! NSDate
-    cell.textLabel!.text = object.description
+    let gist = gists[indexPath.row]
+    cell.textLabel?.text = gist.description
+    cell.detailTextLabel?.text = gist.ownerLogin
+    // TODO: set cell.imageView to display image at gist.ownerAvatarURL
+    
     return cell
   }
 
-  override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+  override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath:
+    NSIndexPath) -> Bool {
     // Return false if you do not want the specified item to be editable.
-    return true
+    return false
   }
-
-  override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+  
+  override func tableView(tableView: UITableView, commitEditingStyle editingStyle:
+    UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
     if editingStyle == .Delete {
-        objects.removeAtIndex(indexPath.row)
-        tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+      gists.removeAtIndex(indexPath.row)
+      tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
     } else if editingStyle == .Insert {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+      // Create a new instance of the appropriate class, insert it into the array,
+      // and add a new row to the table view.
     }
   }
 
-
 }
-
