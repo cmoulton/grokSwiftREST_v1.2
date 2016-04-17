9004a5924dc1f463bbda02ad1e50e1d8654cec5d
Christina Moulton <christina@teakmobile.com>
2016-04-17 12:33:18 -0300

    Added LoginViewController to storyboard. Added code file for it and hooked up an IBOutlet to the button

---
+++ grokSwiftREST/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  grokSwiftREST
+//
+//  Created by Christina Moulton on 2016-04-17.
+//  Copyright © 2016 Teak Mobile Inc. All rights reserved.
+//
+
+import Foundation
