e22efa4c5697582a3a4f4fe3a3fdeb68bdf60252
Christina Moulton <christina@teakmobile.com>
2016-04-17 10:16:44 -0300

    Added Basic Auth demos to the GitHubAPIManager

--- grokSwiftREST/GitHubAPIManager.swift
+++ grokSwiftREST/GitHubAPIManager.swift
@@ -38,6 +38,34 @@ class GitHubAPIManager {
     }
   }
   
+  func doGetWithBasicAuth() -> Void {
+    let username = "myUsername"
+    let password = "myPassword"
+    Alamofire.request(.GET, "https://httpbin.org/basic-auth/\(username)/\(password)")
+      .authenticate(user: username, password: password)
+      .responseString { response in
+        if let receivedString = response.result.value {
+          print(receivedString)
+        }
+    }
+  }
+
+  func doGetWithBasicAuthCredential() -> Void {
+    let username = "myUsername"
+    let password = "myPassword"
+    
+    let credential = NSURLCredential(user: username, password: password,
+                                     persistence: NSURLCredentialPersistence.ForSession)
+    
+    Alamofire.request(.GET, "https://httpbin.org/basic-auth/\(username)/\(password)")
+      .authenticate(usingCredential: credential)
+      .responseString { response in
+        if let receivedString = response.result.value {
+          print(receivedString)
+        }
+    }
+  }
+  
   func fetchGists(urlRequest: URLRequestConvertible, completionHandler:
     (Result<[Gist], NSError>, String?) -> Void) {
     Alamofire.request(urlRequest)
